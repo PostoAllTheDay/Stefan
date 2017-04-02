e9e2c014e56244e4ef4b75c24abfdc98e4e58f15
Stefan Darius Postolache <darius@postolache.net>
2017-04-02 16:46:15 +0300

    If you use this program too much, you'll lag your computer git add .!

---
+++ cringe.py
@@ -0,0 +1,39 @@
+import turtle
+ada = turtle.Pen()
+ada.shape("turtle")
+ada.speed(10)
+for i in range(20):
+    ada.fd(100)
+    ada.lt(70)
+    ada.fd(100)
+    ada.lt(70)
+
+ada.reset()
+ada.down()
+ada.speed(22)
+for j in range(300):
+    ada.fd(i)
+    ada.lt(22222)
+ada.pu()
+ada.fd(1000)
+
+ada.reset()
+ada.down()
+ada.speed(50)
+for k in range(100):
+    for a in range(10):
+        ada.fd(20)
+        ada.lt(70)
+        ada.fd(40)
+        ada.lt(60)
+        ada.fd(60)
+        ada.lt(50)
+        ada.fd(80)
+        ada.lt(40)
+        ada.fd(100)
+        ada.lt(30)
+        ada.fd(120)
+        ada.lt(20)
+        ada.fd(140)
+        ada.lt(10)
+    ada.lt(45)
